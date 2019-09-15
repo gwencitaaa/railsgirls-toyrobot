8828a94164ff5597274b9b73b03022a9af8c18be
gwencitaaa <gwendolen.warnick@gmail.com>
2019-09-15 23:15:55 +1000

    added tests and refactored for readability

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -1,13 +1,34 @@
 class ToyRobot 
-    attr_accessor :position
+  ERROR_MESSAGE = "Invalid direction: please choose North, South, East or West."
+  VALID_DIRECTIONS = %i[north south east west]
+  attr_accessor :position, :direction
 
-    def initialize 
-        @position = [0, 0]
-    end
+  def initialize(x: 0, y: 0, direction: :north) 
+    raise ERROR_MESSAGE unless VALID_DIRECTIONS.include? direction
+    @position = [x, y]
+    @direction = direction
+  end
     
-    def move(x: 0, y: 0)
-        # position[0] += x
-        # position[1] += y
+  def move
+    if direction == :north
+      move_y(1)
+    elsif direction == :south
+      move_y(-1)
+    elsif direction == :east
+      move_x(1)
+    else 
+      move_x(-1) 
     end
+  end
+
+  private
+
+  def move_x(value)
+    position[0] += value
+  end
+  
+  def move_y(value)
+    position[1] += value
+  end
 
 end
--- spec/toy_robot_spec.rb
+++ spec/toy_robot_spec.rb
@@ -1,14 +1,63 @@
 RSpec.describe ToyRobot do
-    subject { ToyRobot.new }
+  subject { described_class.new }
+  
+  describe "#initialize" do 
     
-    describe "#move" do
-        it "changes the position by 1 space" do 
-            expect { subject.move(x: 2) }.to change { subject.position }.from([0, 0]).to([2, 0])
-            #Other way: 
-            #subject.move(2)
-            #expect(subject.position).to eq [2, 0]
+    context "when a position is specified" do 
+      subject { described_class.new(x: 2, y: 3, direction: :north) }
+      it "sets the inital position and direction robot is facing" do
+        aggregate_failures do 
+          expect(subject.position).to eq [2, 3]
+          expect(subject.direction).to eq :north
         end
+      end
+    end
+    
+    context "when no position is specified" do
+      it "starts at the origin facing North" do
+        aggregate_failures do 
+          expect(subject.position).to eq [0, 0]
+          expect(subject.direction).to eq :north
+        end
+      end
+    end
 
-    end 
+    context "when an invalid direction is given" do
+      it "raises an error" do
+        expect { described_class.new(direction: :foo) }.to raise_error
+      end
+    end
+  end
+  
+  describe "#move" do
+
+    context "when robot is facing north" do
+      subject { described_class.new(direction: :north) }
+      it "moves up by 1 space" do 
+        expect { subject.move }.to change { subject.position }.from([0, 0]).to([0, 1])
+      end
+    end
+    
+    context "when robot is facing south" do
+      subject { described_class.new(y: 2, direction: :south) }
+      it "moves down by 1 space" do 
+        expect { subject.move }.to change { subject.position }.from([0, 2]).to([0, 1])
+      end
+    end
+    
+    context "when robot is facing east" do
+      subject { described_class.new(direction: :east) }
+      it "moves right by 1 space" do 
+        expect { subject.move }.to change { subject.position }.from([0, 0]).to([1, 0])
+      end
+    end
+    
+    context "when robot is facing west" do
+      subject { described_class.new(x: 2, direction: :west) }
+      it "moves left by 1 space" do 
+        expect { subject.move }.to change { subject.position }.from([2, 0]).to([1, 0])
+      end
+    end
+  end 
 
 end
